ae166befe346bf100e76d2867085ce486a3f6f03
JosueRM2001 <134113256+JosueRM2001@users.noreply.github.com>
2024-11-08 22:58:55 -0500

    New File

---
+++ format.js
@@ -0,0 +1,7 @@
+
+console.log("Welcome, this is a JavaScript program.");
+// Change the background color when clicking anywhere on the page
+document.body.addEventListener("click", () => {
+    document.body.style.backgroundColor = "#FFD700"; // Change to gold color
+});
+
